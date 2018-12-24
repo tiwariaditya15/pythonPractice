29877bbf5f88bd50f3b0e49b6951ed8a6b57b0c7
Aditya <tiwariaditya15@hotmail.com>
2018-12-25 01:51:33 +0530

    PythonBasics

--- regularExpression.py
+++ regularExpression.py
@@ -1,18 +1,18 @@
 import re
 
-fileContaner = open("one.txt", "r")
+fileContainer = open("one.txt", "r")
 pattern = re.compile('google')
 # using re.search() which returns true or false depending on whether the pattern is available
-def isSearch(fileContaner):
-    for line in fileContaner:
+def isSearch(fileContainer):
+    for line in fileContainer:
         if re.search(pattern, line):
             print(line, end = '\n')
 
 
 #using re.subn() which returns tuple of line read and numbers of replacement done per line
 def isSubn():
-    for line in fileContaner:
+    for line in fileContainer:
         tup, num = re.subn('regular', 'non-linear', line)
         print('The returned string from subn is "{tupl}" and number of replacement of word done are {num}.'.format(tupl = tup, num = num))
 
-isSearch(fileContaner)
+isSearch(fileContainer)
