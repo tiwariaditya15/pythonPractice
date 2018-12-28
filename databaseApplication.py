e9d2794fcff25eadaed072588bcc8d56a5238961
Aditya <tiwariaditya15@hotmail.com>
2018-12-28 16:28:19 +0530

    PythonBasics

--- databaseApplication.py
+++ databaseApplication.py
@@ -1,3 +1,42 @@
+
+import sqlite3
+
+class Database:
+    def __init__(self, columnName1, columnName2, db):
+        self.name = columnName1
+        self.id = columnName2
+        db.execute('drop table if exists employee')
+        db.execute('create table employee({col1} varchar(5), {col2} int not null)'.format(col1 = self.name, col2 = self.id))
+        db.commit()
+
+    def insertion(self, db, row):
+       db.execute('insert into employee ({col1}, {col2}) values(?, ?)'.format(col1 = self.name, col2 = self.id), (row['ename'], row['eid']))
+       db.commit()
+       print('Inserted Successfully.')
+
+    def retrive(self, pkey, db):
+        cursor = db.execute('select * from employee where {primary} = {val}'.format(primary = self.id, val = pkey))
+        for row in cursor:
+            print(row['ename'])
+    def updation(self): 
+        pass
+    def deletion(self):
+        pass
+
+
+
+def main():
+    
+    db = sqlite3.connect('employee.db')
+    dbclass = Database('ename', 'eid', db)
+    db.row_factory = sqlite3.Row
+    dbclass.insertion(db, dict(ename = 'aditya', eid = int(45)))
+    dbclass.retrive(45, db)
+
+main()
+
+
+'''
 import sqlite3
 
 def insertInto():
@@ -18,4 +57,5 @@ def showDatabase():
         print(dictRow)
         
 
-showDatabase()
+insertInto()
+'''
